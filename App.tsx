e13c695b979c1614a7b14a6b8577a675bc747564
GlendyT <glendytuyuc@gmail.com>
2024-07-15 15:19:16 -0600

    comprobar estilos en react native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {PaperProvider} from 'react-native-paper';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 //import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
 //import {CounterScreen} from './src/presentation/screens/CounterScreen';
-import {PaperProvider} from 'react-native-paper';
-import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
-
-import IonIcon from 'react-native-vector-icons/Ionicons';
+//import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
+//import {BoxObjectModelScreen} from './src/presentation/screens/BoxObjectModelScreen';
+//import {DimensionScreen} from './src/presentation/screens/DimensionScreen';
+//import {PostitionScreen} from './src/presentation/screens/PostitionScreen';
+//import {FlexScreen} from './src/presentation/screens/FlexScreen';
+import {FlexDirectionScreen} from './src/presentation/screens/FlexDirectionScreen';
 
 export const App = () => {
   return (
@@ -17,7 +21,12 @@ export const App = () => {
       <SafeAreaView style={{flex: 1}}>
         {/*<HelloWorldScreen name="Glendy Tuyuc" />*/}
         {/*<CounterScreen />*/}
-        <CounterM3Screen />
+        {/*<CounterM3Screen />*/}
+        {/*<BoxObjectModelScreen />*/}
+        {/*<DimensionScreen />*/}
+        {/*<PostitionScreen />*/}
+        {/*<FlexScreen />*/}
+        <FlexDirectionScreen />
       </SafeAreaView>
     </PaperProvider>
   );
